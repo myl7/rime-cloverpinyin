dd354bcb35b7fe3181e799ea7b8ebf4c6796f756
myl7 <myl@myl.moe>
2021-12-02 19:35:47 +0800

    build: Rollup version and update build script

--- pack.sh
+++ pack.sh
@@ -8,4 +8,4 @@ rm -rf clover.schema-$ver.zip
 rm -rf clover.schema-build-$ver.zip
 zip -5 clover.schema-$ver.zip *.yaml opencc/*
 zip -5 clover.schema-build-$ver.zip *.yaml opencc/* build/*
-cp clover.schema-build-$ver.zip ../build
+cp clover.schema-build-$ver.zip ../build/clover.schema-$ver.zip
